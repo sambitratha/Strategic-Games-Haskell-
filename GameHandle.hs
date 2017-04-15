767908514d1fdcb4f6ec6f5101508a25f99f3b98
Raaghav0 <es15btech11021@iith.ac.in>
2017-04-15 18:57:07 +0530

    Update GameHandle.hs

--- GameHandle.hs
+++ GameHandle.hs
@@ -6,6 +6,66 @@ import Search
 import Operations
 import Graphics.UI.GLUT	
 
+checkifwon :: [Char] -> [Char] ->Integer -> Bool
+checkifwon board boardstate index =
+	if index == 0 
+		then
+			if getCharacter board index == '0'
+				then 
+					if getCharacter boardstate index == '2'
+						then True
+					else
+						False
+				else
+					if getCharacter boardstate index == '1'
+						then 
+							False
+						else
+							True
+		else
+			if getCharacter board index == '0'
+				then
+					if getCharacter boardstate index == '2'
+						then 
+							checkifwon board boardstate (index -1)
+
+						else
+							False
+				else
+					if getCharacter boardstate index == '0'
+						then 
+							checkifwon board boardstate (index -1)
+						else
+							False
+
+changeboardstate :: [Char] -> [Char] -> Integer -> [Char]
+changeboardstate board boardstate index = 
+	if index == 0
+		then
+			if getCharacter board index == '0'
+				then
+					setElement boardstate index '0'
+
+				else
+					boardstate
+
+		else
+			if getCharacter board index == '0'
+				then 
+					changeboardstate board (setElement boardstate index '0') (index -1)
+
+				else
+					changeboardstate board boardstate (index-1)
+
+getTotalMines::[Char] -> Integer -> Integer->Integer
+getTotalMines board 0 rs = rs
+getTotalMines board index rs =
+	if getCharacter board (index -1) == '0'
+		then
+			getTotalMines board (index -1) (rs +1)
+		else
+			getTotalMines board (index-1) rs
+
 modify_boardstate :: Bool -> Bool -> IORef [Char] -> IORef [Char] -> Integer -> IO () -- modifies the boardstate list
 modify_boardstate left_click flag board1 _boardstate i = do
 	board <- readIORef board1
@@ -39,6 +99,26 @@ modify_boardstate left_click flag board1 _boardstate i = do
 					let c = setElement boardstate i '2'
 					_boardstate $~! \x -> c
 	else do_nothing
+
+modifyBoard::[Char] -> Integer ->  Integer -> Integer -> [Char]
+modifyBoard board length index no_of_mines =
+	if length == no_of_mines
+		then
+			board
+		else
+			if length > no_of_mines
+				then
+					if getCharacter board index == '0'
+						then
+							modifyBoard (setElement board index '1') (length - 1) ( (index+3) `mod` 400) no_of_mines
+						else
+							modifyBoard board length ( (index + 1) `mod` 400) no_of_mines
+				else
+					if getCharacter board index == '0'
+						then 
+							modifyBoard board length ( (index + 1) `mod` 400) no_of_mines
+						else
+							modifyBoard (setElement board index '0') (length + 1) ( (index + 3) `mod` 400) no_of_mines
 
 do_nothing ::  IO ()
 do_nothing = return ()
